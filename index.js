a09c3534922651b0658271bf3455c28201aae10f
Abrar <153528444+abrar1701@users.noreply.github.com>
2026-02-26 22:17:49 +0530

    added feature to add file to array

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import multer from "multer";
 
-let aee = [];
+let arr = [];
 
 const app = express();
 const port = 3000;
@@ -16,15 +16,48 @@ app.listen(port, ()=> {
 });
 
 app.get('/', (req, res) => {
-    res.render("index.ejs")
+    res.render('home.ejs');
 });
 
-app.get('/yuvan', (req, res) => {
-    res.send("Hello i am yuvan");
+app.get('/profile', (req, res) => {
+    res.render('profile.ejs');
 });
 
-app.post('/abrar', upload.single('img'), (req, res) => {
-    console.log(req.file);
-    res.set('Content-Type', req.file.mimetype);
-    res.send(req.file.buffer);
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
 });
+
+app.get('/post', (req, res) => {
+    res.render('index.ejs');
+});
+
+
+app.post('/login', (req, res) => {
+    res.render('login.ejs');
+});
+
+app.post('/post', upload.single('img'), (req, res) => {
+    arr.push({
+        name: req.body.title,
+        description: req.body.description,
+        file: `data:${req.file.mimetype};base64,${req.file.buffer.toString('base64')}`
+    });
+    console.log(arr[0]);
+    res.render('post.ejs');
+});
+
+
+
+// app.get('/', (req, res) => {
+//     res.render("index.ejs")
+// });
+
+// app.get('/yuvan', (req, res) => {
+//     res.send("Hello i am yuvan");
+// });
+
+// app.post('/abrar', upload.single('img'), (req, res) => {
+//     console.log(req.file);
+//     res.set('Content-Type', req.file.mimetype);
+//     res.send(req.file.buffer);
+// });
